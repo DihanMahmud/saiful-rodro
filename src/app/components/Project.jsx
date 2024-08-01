133bd3f69ed5ab09b800913e35d7a8a3e4acbbc6
Dihan Mahmud <dihantapu@gmail.com>
2024-08-01 15:47:32 +0600

    font-size change

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -23,7 +23,7 @@ const items = [
 export default function Project () {
   return (
     <div id="portfolio" className="max-w-screen-2xl px-6 md:px-12 mx-auto py-24">
-      <h1 className=" uppercase pb-12 text-7xl text-center text-[#7cfc00]">Portfolio</h1>
+      <h1 className=" uppercase pb-12 text-4xl sm:text-7xl text-center text-[#7cfc00]">Portfolio</h1>
       <Card items={items} />
     </div>
   );
--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -3,7 +3,7 @@ import Icon from "./Icon";
 export default function Skills() {
   return (
     <div className="max-w-screen-2xl mx-auto px-6 pt-12 md:px-12">
-        <h1 className="text-center uppercase text-7xl text-[#7CFC00] pb-7">My Skills</h1>
+        <h1 className="text-center uppercase text-4xl sm:text-7xl text-[#7CFC00] pb-7">My Skills</h1>
         <div className=" flex flex-col justify-center items-center gap-6">
         <div className="flex gap-9 lg:flex-nowrap flex-wrap justify-center">
         <Icon></Icon>
