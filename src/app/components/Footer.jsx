8e6a89d0a2a24c77f021e4c720e6900673e89844
Dihan Mahmud <dihantapu@gmail.com>
2024-08-01 18:14:07 +0600

    dihanmahmud.me

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <p className=" mt-5 text-xs text-[#d1d1d1]">
         &copy; {new Date().getFullYear()} Saiful Islam. All rights reserved.
       </p>
-      <p className=" mt-5 text-xs text-[#d1d1d1]">Designed by <Link className=" underline" href='https://www.linkedin.com/in/dihan-mahmud'> Dihan Mahmud.</Link></p>
+      <p className=" mt-5 text-xs text-[#d1d1d1]">Designed by <Link className=" underline" target="_blank" href='https://www.dihanmahmud.me/'> Dihan Mahmud.</Link></p>
     </div>
   );
 }
